dfbf9c13d249c766f4092f0deb5b010080120377
Aadithya Niranjan <aadiniranjan27@gmail.com>
2022-03-01 21:20:32 +0530

    DRUMS MUSIC WEB

---
+++ JAVASCRIPT/drums.js
@@ -0,0 +1,29 @@
+document.querySelector(".w-drum").addEventListener("click", function() {
+
+  var audio = new Audio('D:/WEB DEVELOPMT ATOM CODE/Drum Kit Starting Files/sounds/crash.mp3');
+  audio.play();
+});
+document.querySelector(".d-drum").addEventListener("click", function() {
+  var audio1 = new Audio('D:/WEB DEVELOPMT ATOM CODE/Drum Kit Starting Files/sounds/kick-bass.mp3');
+  audio1.play();
+});
+document.querySelector(".s-drum").addEventListener("click", function() {
+var audio2 = new Audio('D:/WEB DEVELOPMT ATOM CODE/Drum Kit Starting Files/sounds/snare.mp3');
+audio2.play();
+});
+document.querySelector(".j-drum").addEventListener("click", function() {
+  var audio4 = new Audio('D:/WEB DEVELOPMT ATOM CODE/Drum Kit Starting Files/sounds/tom-2.mp3');
+  audio4.play();
+});
+document.querySelector(".k-drum").addEventListener("click", function() {
+  var audio5 = new Audio('D:/WEB DEVELOPMT ATOM CODE/Drum Kit Starting Files/sounds/tom-3.mp3');
+  audio5.play();
+});
+document.querySelector(".l-drum").addEventListener("click", function() {
+  var audio6 = new Audio('D:/WEB DEVELOPMT ATOM CODE/Drum Kit Starting Files/sounds/tom-4.mp3');
+  audio6.play();
+});
+document.querySelector(".a-drum").addEventListener("click", function() {
+  var audio6 = new Audio('D:/WEB DEVELOPMT ATOM CODE/Drum Kit Starting Files/sounds/tom-1.mp3');
+  audio6.play();
+});
